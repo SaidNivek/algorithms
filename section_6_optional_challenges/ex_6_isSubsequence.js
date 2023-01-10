8ba94623aa24abea5a92c0573fa81b7861c56adf
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-10 12:01:46 -0500

    Notes added to ex_6

--- section_6_optional_challenges/ex_6_isSubsequence.js
+++ section_6_optional_challenges/ex_6_isSubsequence.js
@@ -7,12 +7,16 @@ function isSubsequence(str1, str2) {
     let pointer1 = 0
     // Set str2 point to first letter in the 2nd string
     let pointer2 = 0
-    // While pointer is less than str2.length, compare the numbers
+    // While pointer2 is less than str2.length, compare the numbers
     while(pointer2 < str2.length) {
+        // If the value at str2[pointer2] is equal to the value at str1[pointer1], increment pointer1 up by 1
         if(str2[pointer2] === str1[pointer1]) pointer1++
+        // If pointer1 is ever the same number as str1.length, return true immediately, since we have passed beyond the string and, therefore, all values have been matched in order
         if (pointer1 === str1.length) return true
+        // Increment pointer2 every time through, to eventually fulfill the while condition
         pointer2++
     }
+    // If the while condition is escaped from, return false, since the true condition was never met
     return false   
   }
 
