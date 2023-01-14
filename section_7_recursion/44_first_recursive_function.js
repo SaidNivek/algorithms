0c5083b7156f03005528edbaa2b1939061cf7ff8
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-14 13:29:18 -0500

    Begin recursion

---
+++ section_7_recursion/44_first_recursive_function.js
@@ -0,0 +1,4 @@
+// All recursive functions need two things:
+// 1 - the base case - the condition where the recursion ends and stops calling itself
+// 2 - Different input - basically the recursive call that uses a DIFFERENT piece of data because it needs to change in order for the base case to be met
+
