fee0e96ba950a20cc66d2ad78f66b457b9900c09
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-14 13:34:40 -0500

    First recursion example complete

--- section_7_recursion/44_first_recursive_function.js
+++ section_7_recursion/44_first_recursive_function.js
@@ -2,3 +2,14 @@
 // 1 - the base case - the condition where the recursion ends and stops calling itself
 // 2 - Different input - basically the recursive call that uses a DIFFERENT piece of data because it needs to change in order for the base case to be met
 
+function countDown(num) {
+    if(num <= 0) {
+        console.log("All done!")
+        return
+    }
+    console.log(num)
+    num--
+    countDown(num)
+}
+
+countDown(10)
