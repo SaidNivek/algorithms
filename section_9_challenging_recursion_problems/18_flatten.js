28b2cd32136f371496801468a445d8999691bef4
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-27 18:50:21 -0500

    21_capitalizeWords recursion complete

--- section_9_challenging_recursion_problems/18_flatten.js
+++ section_9_challenging_recursion_problems/18_flatten.js
@@ -13,6 +13,6 @@ return newArr;
 
   
 console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
-console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
-console.log(flatten([[1],[2],[3]])) // [1,2,3]
-console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
+// console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+// console.log(flatten([[1],[2],[3]])) // [1,2,3]
+// console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
