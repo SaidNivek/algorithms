d82ad333757e0571ea925c8b439d5601ee69cf85
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-22 23:12:37 -0400

    Add comments for pseudo code

--- section_10_searching_algorithms/naive_string_search.js
+++ section_10_searching_algorithms/naive_string_search.js
@@ -1,5 +1,11 @@
 function stringSearch(string, search) {
     let count = 0
+    // Get the length of the second string
+
+    // While the 1st string is larger than or equal to the search string, go through the first string
+    // Check to see if the characters in the current first position match the current letter of the search string
+    // If the string has a match, then we iterate the count up and reset the first position to the next char in the string
+    // If they don't match, then we reset to the next char in the string
 
     return count
 }
