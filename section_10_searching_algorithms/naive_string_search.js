a3c68aeec198a565d9c69809749f87cb3f59bd46
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-23 22:50:21 -0400

    Finish naive string search

--- section_10_searching_algorithms/naive_string_search.js
+++ section_10_searching_algorithms/naive_string_search.js
@@ -2,13 +2,24 @@ function stringSearch(string, search) {
     let count = 0
     // Get the length of the second string
     let searchNum = search.length
-    console.log(string.length - searchNum)
+    
     let start = 0
+    let check = 0
     // For loop only needs to go up to string.length - searchNum beacause if the string is less than the length of the search, then it cannot be a match
-    for(let i = 0; i < string.length - searchNum; i++) {
-
+    while (start < string.length - searchNum) {
+        for(let i = start; i < start + searchNum; i++) {
+            if(string[i] === search[check]) check++
+            else {
+                check = 0
+                start++
+                break
+            }
+            if(check === searchNum) {
+                count++
+                start++
+            }
+        }
     }
-
     // While the 1st string is larger than or equal to the search string, go through the first string
     // Check to see if the characters in the current first position match the current letter of the search string
     // If the string has a match, then we iterate the count up and reset the first position to the next char in the string
@@ -18,4 +29,4 @@ function stringSearch(string, search) {
     return count
 }
 
-console.log(stringSearch('test', 'est'))
+console.log(stringSearch('cherries', 'snow'))
