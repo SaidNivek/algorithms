d3c2fc161c00fe1a2e904fd149ed426179f79074
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-28 15:13:48 -0400

    Fix some bugs in algorithm

--- section_12_selection_sort/75_selection_sort.js
+++ section_12_selection_sort/75_selection_sort.js
@@ -1,19 +1,22 @@
 function selectionSort(arr) {
-    // Start a loop and keep track of the position and lowest number found
+    // Start a loop and keep track of the current position in the array and position of the lowest number found
     let current = 0
     let lowest = 0
+    // Set a swapped value to false that will allow us to exit the loop earlier if there are no swaps
     let swapped = false
     do {
         swapped = false
         for(let i = current; i < arr.length; i++) {
-            console.log(current, lowest)
-            if(arr[i] < arr[lowest]) lowest = i
+            console.log(current, lowest, arr[lowest])
+            if(arr[i] < arr[lowest]) {
+                lowest = i
+                swapped = true
+            }
             if(i === arr.length - 1) {
                 let temp = arr.splice(lowest, 1)
-                arr.splice(current, 0, temp[0])
-                swapped = true
+                arr.splice(current, 0, temp[0])                
                 current++
-                lowest = current
+                lowest = current + 1
             }
         }
     } while (swapped)
@@ -22,3 +25,4 @@ function selectionSort(arr) {
 
 const anArray = [15, 2, 90, 87, 40, 36, 28, 11, 80, 70, 20, 22, 19]
 console.log(selectionSort(anArray))
+console.log(selectionSort([1,2,3]))
