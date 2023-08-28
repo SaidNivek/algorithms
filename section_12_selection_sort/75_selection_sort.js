b4328d75c2bacb911fe5d25c965f38fdd275387d
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-28 15:21:59 -0400

    Add code from videos to file

--- section_12_selection_sort/75_selection_sort.js
+++ section_12_selection_sort/75_selection_sort.js
@@ -26,3 +26,42 @@ function selectionSort(arr) {
 const anArray = [15, 2, 90, 87, 40, 36, 28, 11, 80, 70, 20, 22, 19]
 console.log(selectionSort(anArray))
 console.log(selectionSort([1,2,3]))
+
+// // LEGACY VERSION (non ES2015 syntax)
+// function sselectionSort(arr){
+//     for(var i = 0; i < arr.length; i++){
+//         var lowest = i;
+//         for(var j = i+1; j < arr.length; j++){
+//             if(arr[j] < arr[lowest]){
+//                 lowest = j;
+//             }
+//         }
+//         if(i !== lowest){
+//             //SWAP!
+//             var temp = arr[i];
+//             arr[i] = arr[lowest];
+//             arr[lowest] = temp;
+//         }
+//     }
+//     return arr;
+// }
+
+// // ES2015 VERSION
+// function selectionSort(arr) {
+//   const swap = (arr, idx1, idx2) =>
+//     ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let lowest = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[lowest] > arr[j]) {
+//         lowest = j;
+//       }
+//     }
+//     if (i !== lowest) swap(arr, i, lowest);
+//   }
+
+//   return arr;
+// }
+
+// selectionSort([0,2,34,22,10,19,17]);
