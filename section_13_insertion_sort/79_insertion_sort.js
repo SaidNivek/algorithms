945ad7ab2c2219e6d5553189c5ac0f59fef0e811
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-29 21:19:29 -0400

    Begin insertion sort

---
+++ section_13_insertion_sort/79_insertion_sort.js
@@ -0,0 +1,8 @@
+function insertionSort(arr) {
+   
+    return arr
+}
+
+const anArray = [15, 2, 90, 87, 40, 36, 28, 11, 80, 70, 20, 22, 19]
+console.log(insertionSort(anArray))
+// console.log(insertionSort([1,2,3]))
