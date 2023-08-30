90a5b0170c6de370974ac47903d0719458a70980
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-29 21:49:04 -0400

    Add code from video as comment

--- section_13_insertion_sort/79_insertion_sort.js
+++ section_13_insertion_sort/79_insertion_sort.js
@@ -23,3 +23,17 @@ function insertionSort(arr) {
 const anArray = [15, 2, 90, 87, 40, 36, 28, 11, 80, 70, 20, 22, 19]
 console.log(insertionSort(anArray))
 console.log(insertionSort([1,2,3]))
+
+// function insertionSort(arr){
+// 	var currentVal;
+//     for(var i = 1; i < arr.length; i++){
+//         currentVal = arr[i];
+//         for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+//             arr[j+1] = arr[j]
+//         }
+//         arr[j+1] = currentVal;
+//     }
+//     return arr;
+// }
+
+// insertionSort([2,1,9,76,4])
