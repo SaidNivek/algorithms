6ad406a96cbea776f3924b033c0e0a98a94c50e8
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-29 21:44:58 -0400

    Finish insertion sort algo

--- section_13_insertion_sort/79_insertion_sort.js
+++ section_13_insertion_sort/79_insertion_sort.js
@@ -1,8 +1,25 @@
 function insertionSort(arr) {
-   
+    for(let i = 1; i < arr.length; i++) {
+        if(i === 1) {
+            if(arr[i] < arr[i - 1]) {
+                const temp = arr.splice(i, 1)[0]
+                arr.splice(0, 0, temp)
+            }
+        } else {
+            if(arr[i] < arr[i - 1]) {
+                for(let j = 0; j < i; j++) {
+                    if(arr[i] < arr[j]) {
+                        const temp = arr.splice(i, 1)[0]
+                        arr.splice(j, 0, temp)
+                    }
+                }
+            }
+        }
+
+    }
     return arr
 }
 
 const anArray = [15, 2, 90, 87, 40, 36, 28, 11, 80, 70, 20, 22, 19]
 console.log(insertionSort(anArray))
-// console.log(insertionSort([1,2,3]))
+console.log(insertionSort([1,2,3]))
