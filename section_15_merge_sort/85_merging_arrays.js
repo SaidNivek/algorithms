aac61945c89e6842576b5db92969ea4f2ae51f4c
SaidNivek <kevin.vi.dias@gmail.com>
2023-09-18 21:38:32 -0400

    mergeSort helper function complete

--- section_15_merge_sort/85_merging_arrays.js
+++ section_15_merge_sort/85_merging_arrays.js
@@ -1,9 +1,27 @@
 function mergeArrays(arr1, arr2) {
-    return (console.log('mergeArrays function'))
+    let newArray = []
+    let val1 = 0
+    let val2 = 0
+
+    while(val1 < arr1.length && val2 < arr2.length) {
+        if (arr1[val1] <= arr2[val2]) {
+            newArray.push(arr1[val1])
+            val1++
+        } else {
+            newArray.push(arr2[val2])
+            val2++
+        }
+        if (val1 === arr1.length) newArray = [...newArray, ...arr2.splice(val2, (arr2.length - val2))]
+        if (val2 === arr2.length) newArray = [...newArray, ...arr1.splice(val1, (arr1.length - val1))]
+    }
+
+    return newArray
 }
 
-function mergeSort() {
-    return mergeArrays()
+function mergeSort(arr1, arr2) {
+    return mergeArrays(arr1, arr2)
 }
 
-mergeSort()
+let arr1 = [4, 8, 8, 9, 11, 12, 19, 34]
+let arr2 = [1, 3, 7, 70, 74]
+console.log(mergeSort(arr1, arr2))
