d3efd07f58ddea5a0a4a0364d513b4642bc97af6
SaidNivek <kevin.vi.dias@gmail.com>
2023-09-30 20:35:23 -0400

    Pseudocode for merge sort

--- section_15_merge_sort/85_merging_arrays.js
+++ section_15_merge_sort/85_merging_arrays.js
@@ -18,6 +18,11 @@ function mergeArrays(arr1, arr2) {
     return newArray
 }
 
+// Break up the array into halves until you have arrays that are empty or have one element
+// Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+// Once the array has been merged back together, return the merged (and sorted!) array
+// USES RECURSION!
+
 function mergeSort(arr1, arr2) {
     return mergeArrays(arr1, arr2)
 }
